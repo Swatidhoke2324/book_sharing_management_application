eabd4981474e34c710a37a5c63c77b57b670fb46
suyash0102 <59247598+suyash0102@users.noreply.github.com>
2021-06-03 09:36:32 +0530

    conflicts

--- lib/books/details/details_screen.dart
+++ lib/books/details/details_screen.dart
@@ -2,7 +2,6 @@ import 'package:book_sharing_management_application/data.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import '../../constants.dart';
 import '../constants.dart';
 import '../all_book.dart';
 import 'components/body.dart';
@@ -17,6 +16,7 @@ class DetailsScreen extends StatefulWidget {
   final bookPostedBy;
   final typeOfBook;
 
+<<<<<<< HEAD
   const DetailsScreen(
       {Key key,
       this.product,
@@ -28,12 +28,43 @@ class DetailsScreen extends StatefulWidget {
       this.bookPostedBy,
       this.typeOfBook})
       : super(key: key);
+=======
+  const DetailsScreen({Key key, this.product, this.bookName, this.type, this.imageUrl, this.authorName, this.reDirect, this.bookPostedBy, this.typeOfBook}) : super(key: key);
+
+  @override
+  _DetailsScreenState createState() => _DetailsScreenState();
+}
+
+class _DetailsScreenState extends State<DetailsScreen> {
+  @override
+  void initState() {
+    getData();
+    // TODO: implement initState
+    super.initState();
+  }
+  void getData(){
+    FirebaseFirestore.instance
+        .collection('UserDetails')
+        .doc(widget.bookPostedBy)
+        .get()
+        .then((DocumentSnapshot documentSnapshot) {
+      if (documentSnapshot.exists) {
+        lenderDetails = documentSnapshot.data();
+        lenderEmail=lenderDetails["E-Mail"];
+        lenderName=lenderDetails["UserName"];
+        lenderPhoneNo=lenderDetails["PhoneNo."];
+      }
+      print(lenderEmail);
+    });
+  }
+>>>>>>> parent of 58e121a (Formatting Changes)
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       // each product have a color
       backgroundColor: Colors.grey,
       appBar: buildAppBar(context),
+<<<<<<< HEAD
       body: Body(
         product: product,
         bookName: bookName,
@@ -44,13 +75,15 @@ class DetailsScreen extends StatefulWidget {
         typeOfBook: typeOfBook,
         bookPostedBy: bookPostedBy,
       ),
+=======
+      body: Body(product: widget.product,bookName: widget.bookName,type: widget.type,imageUrl: widget.imageUrl,authorName: widget.authorName,reDirect: widget.reDirect,typeOfBook: widget.typeOfBook,bookPostedBy: widget.bookPostedBy,),
+>>>>>>> parent of 58e121a (Formatting Changes)
     );
   }
 
   AppBar buildAppBar(BuildContext context) {
     return AppBar(
-      backgroundColor: kPrimaryColor,
-      title: Text("Book Details"),
+      backgroundColor:Colors.blue,
       elevation: 0,
       leading: IconButton(
         icon: SvgPicture.asset(
